c66065df1b9fa36fd6eba2a68c90f30a8b477b36
devhorn <mdevhorn@gmail.com>
2025-03-31 20:41:04 +0200

    designed how to order section

--- src/app/main-content/landing-page/landingPage.component.ts
+++ src/app/main-content/landing-page/landingPage.component.ts
@@ -6,17 +6,7 @@ import { NavbarComponent } from './navbar/navbar/navbar.component';
   selector: 'app-landingPage',
   standalone: true,
   imports: [CommonModule, NavbarComponent],
-  template: `
-    <section>
-      <div class="fontRaleway">
-        <app-navbar></app-navbar>
-      </div>
-      <div class="sakuraTitle">
-        <h1 class="fontRaleway">SAKURA RAMEN</h1>
-        <h2 class="fontRaleway">THE BEST RAMEN IN TOWN</h2>
-      </div>
-    </section>
-  `,
+  templateUrl: './landingPage.component.html',
   styleUrls: ['./landingPage.component.scss'],
 })
 export class LandingPageComponent {}
--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingPageComponent } from './landing-page/landingPage.component';
+import { HowToOrderComponent } from './how-to-order/how-to-order.component';
 import { OurMenuComponent } from './our-menu/our-menu.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [CommonModule, LandingPageComponent, OurMenuComponent],
+  imports: [
+    CommonModule,
+    LandingPageComponent,
+    HowToOrderComponent,
+    OurMenuComponent,
+  ],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss',
 })
