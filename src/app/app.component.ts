92e4745a8d325ca6353a337141aa1fd1e450a8e1
devhorn <mdevhorn@gmail.com>
2025-04-01 08:31:46 +0200

    implment routes for imprint and maincontent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,18 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MainContentComponent } from './main-content/main-content.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { ImprintComponent } from './imprint/imprint.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MainContentComponent, FooterComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MainContentComponent,
+    FooterComponent,
+    ImprintComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
